1d193b10a5f8a3e940f983406810ead7cb8a353e
SharkFin <sharkfinpro@hotmail.com>
2018-03-06 22:28:08 -0800

    cleanup

--- source/index.js
+++ source/index.js
@@ -8,17 +8,13 @@ const log = message => {
 
 module.exports = class server {
     constructor(config) {
-        this.r = 0;
         this.config = config;
         this.childProcessManager = new childProcessManager(config, this);
         this.ioServer = new socketServer(config, this);
         this.entityServer = new entityServer(config, this);
-
-        this.id = 1;
     }
 
     launch() {
-        this.r++;
         this.ioServer.init();
         log(`Server listening on port ${this.config.port}!`);
         //setTimeout(()=>this.childProcessManager.endProcess('tankUpdates'), 1000)
